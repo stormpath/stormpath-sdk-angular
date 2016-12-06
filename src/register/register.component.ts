cd0d377928ff20741278b1fda5a8a4fda09e648c
Matt Raible <matt@raibledesigns.com>
2016-12-06 09:52:15 -0700

    fix(templates): Fixed 404 in templates by embedding them

--- src/register/register.component.ts
+++ src/register/register.component.ts
@@ -7,7 +7,29 @@ import { Stormpath, LoginFormModel, RegistrationFormModel } from '../stormpath/s
 
 @Component({
   selector: 'register-form',
-  templateUrl: './register.component.html'
+  template: `<template #defaultTemplate>
+  <form *ngIf="!registered" (ngSubmit)="onSubmit()" class="form-horizontal">
+    <div class="form-group" *ngFor="let field of model?.form?.fields">
+      <label [attr.for]="field.name" class="col-sm-4 control-label">{{field.label}}</label>
+      <div class="col-sm-8">
+        <input class="form-control" [name]="field.name" [id]="field.name" [type]="field.type"
+               [(ngModel)]="formModel[field.name]" [placeholder]="field.placeholder" [disabled]="creating" [required]="field.required">
+      </div>
+    </div>
+    <div *ngIf="error" class="alert alert-danger">{{error}}</div>
+    <button type="submit" class="btn btn-primary">Register</button>
+  </form>
+  <p *ngIf="unverified" class="alert alert-success">
+    Your account has been created and requires verification.
+    Please check your email for a verification link.
+  </p>
+  <p class="alert alert-success" *ngIf="canLogin">
+    Your account has been created, you may now log in.
+  </p>
+</template>
+<template
+  [ngTemplateOutlet]="customTemplate || defaultTemplate">
+</template>`
 })
 @Injectable()
 export class RegisterComponent implements OnInit {
