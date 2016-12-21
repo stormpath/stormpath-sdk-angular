8224b015acdb3fa622a808c271f135dc627f68ca
Matt Raible <matt@raibledesigns.com>
2016-12-21 12:46:19 -0700

    revert(demo): Revert change to customize endpoint in demo

--- demo/demo.module.ts
+++ demo/demo.module.ts
@@ -3,18 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { StormpathModule } from '../src/stormpath.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { StormpathConfiguration } from '../src/stormpath/stormpath.config';
-
-let config: StormpathConfiguration = new StormpathConfiguration();
-config.endpointPrefix = 'https://raible.apps.stormpath.io';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, StormpathModule],
   bootstrap: [AppComponent],
-  providers: [{
-    provide: StormpathConfiguration, useValue: config
-  }]
+  providers: []
 })
 export class DemoModule {
 }
