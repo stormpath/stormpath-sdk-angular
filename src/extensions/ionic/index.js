a3e837bbf7dbc38de07ca307767479cd715088f0
Matt Raible <matt@raibledesigns.com>
2017-02-03 17:36:42 -0700

    fix(dependencies): Remove Ionic pages and module

---
+++ src/extensions/ionic/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require('./forgot'));
+__export(require('./login'));
+__export(require('./register'));
--- src/extensions/ionic/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './forgot';
-export * from './login';
-export * from './register';
